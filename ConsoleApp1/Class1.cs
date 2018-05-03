163b6f6fe0670f727fdff48c2b0c7542320ab375
yossi birenboim <yossi1316@gmail.com>
2018-05-03 12:32:46 +0300

    aaaa

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SingleTon
+    {
+        private SingleTon() { }
+        public static SingleTon instance;
+        public static SingleTon Instance
+        {
+            get {
+                if (instance == null)
+                    instance = new SingleTon();
+                return instance;
+            }
+        }
+
+    }
+}
